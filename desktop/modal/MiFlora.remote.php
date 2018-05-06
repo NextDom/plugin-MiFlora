7bf99e02905eb8cc7f71760d638e2f4ecce212d2
Rene Jullien <rene.jullien@gmail.com>
2018-05-06 21:35:44 +0200

    remove trace

--- desktop/modal/MiFlora.remote.php
+++ desktop/modal/MiFlora.remote.php
@@ -27,73 +27,86 @@ sendVarToJS('plugin', $id);
 <div id='div_MiFloraRemoteAlert' style="display: none;"></div>
 <div class="row row-overflow">
 
-	 <div class="col-lg-10 col-md-9 col-sm-8 col-xs-8 remoteThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-<legend><i class="fa fa-table"></i>  {{Mes Antennes}}</legend>
+    <div class="col-lg-10 col-md-9 col-sm-8 col-xs-8 remoteThumbnailDisplay"
+         style="border-left: solid 1px #EEE; padding-left: 25px;">
+        <legend><i class="fa fa-table"></i> {{Mes Antennes}}</legend>
 
-<div class="eqLogicThumbnailContainer">
-	<div class="cursor MiFloraRemoteAction pull-left" data-action="add" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-     <center>
-      <i class="fa fa-plus-circle" style="font-size : 9em;color:#94ca02;"></i>
-    </center>
-    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>{{Ajouter}}</center></span>
-  </div>
-  <?php
-foreach ($remotes as $remote) {
-	echo '<div class="eqLogicDisplayCard cursor pull-left" data-remote_id="' . $remote->getId() . '" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
-	echo "<center>";
-	echo '<img class="lazy" src="plugins/MiFlora/plugin_info/antenna.png" height="105" width="95" />';
-	echo "</center>";
-	echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02""><center>' . $remote->getRemoteName() . '</center></span>';
-	echo '</div>';
-}
-?>
-</div>
-</div>
+        <div class="eqLogicThumbnailContainer">
+            <div class="cursor MiFloraRemoteAction pull-left" data-action="add"
+                 style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+                <center>
+                    <i class="fa fa-plus-circle" style="font-size : 9em;color:#94ca02;"></i>
+                </center>
+                <span
+                    style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>{{Ajouter}}</center></span>
+            </div>
+            <?php
+            foreach ($remotes as $remote) {
+                echo '<div class="eqLogicDisplayCard cursor pull-left" data-remote_id="' . $remote->getId() . '" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+                echo "<center>";
+                echo '<img class="lazy" src="plugins/MiFlora/plugin_info/antenna.png" height="105" width="95" />';
+                echo "</center>";
+                echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02""><center>' . $remote->getRemoteName() . '</center></span>';
+                echo '</div>';
+            }
+            ?>
+        </div>
+    </div>
 
-	<div class="col-lg-10 col-md-9 col-sm-8 col-xs-8 MiFloraRemote" style="border-left: solid 1px #EEE; padding-left: 25px;display:none;">
-		<a class="btn btn-success MiFloraRemoteAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-		<a class="btn btn-danger MiFloraRemoteAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+    <div class="col-lg-10 col-md-9 col-sm-8 col-xs-8 MiFloraRemote"
+         style="border-left: solid 1px #EEE; padding-left: 25px;display:none;">
+        <a class="btn btn-success MiFloraRemoteAction pull-right" data-action="save"><i class="fa fa-check-circle"></i>
+            {{Sauvegarder}}</a>
+        <a class="btn btn-danger MiFloraRemoteAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i>
+            {{Supprimer}}</a>
 
-			<form class="form-horizontal">
-					<fieldset>
-						<legend><i class="fa fa-arrow-circle-left returnAction cursor"></i> {{Général}}</legend>
-						<div class="form-group">
-							<label class="col-sm-2 control-label">{{Nom}}</label>
-							<div class="col-sm-3">
-								<input type="text" class="MiFloraRemoteAttr form-control" data-l1key="id" style="display : none;" />
-								<input type="text" class="MiFloraRemoteAttr form-control" data-l1key="remoteName" placeholder="{{Nom de l'antenne}}"/>
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="col-sm-2 control-label">{{Ip}}</label>
-							<div class="col-sm-3">
-								<input type="text" class="MiFloraRemoteAttr form-control" data-l1key="configuration" data-l2key="remoteIp"/>
-							</div>
-							<label class="col-sm-1 control-label">{{Port}}</label>
-							<div class="col-sm-3">
-								<input type="text" class="MiFloraRemoteAttr form-control" data-l1key="configuration" data-l2key="remotePort"/>
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="col-sm-2 control-label">{{User}}</label>
-							<div class="col-sm-3">
-								<input type="text" class="MiFloraRemoteAttr form-control" data-l1key="configuration" data-l2key="remoteUser"/>
-							</div>
-							<label class="col-sm-1 control-label">{{Password}}</label>
-							<div class="col-sm-3">
-								<input type="password" class="MiFloraRemoteAttr form-control" data-l1key="configuration" data-l2key="remotePassword"/>
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="col-sm-2 control-label">{{Device}}</label>
-							<div class="col-sm-3">
-								<input type="text" class="MiFloraRemoteAttr form-control" data-l1key="configuration" data-l2key="remoteDevice" placeholder="{{ex : hci0}}"/>
-							</div>
-						</div>
+        <form class="form-horizontal">
+            <fieldset>
+                <legend><i class="fa fa-arrow-circle-left returnAction cursor"></i> {{Général}}</legend>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">{{Nom}}</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="MiFloraRemoteAttr form-control" data-l1key="id"
+                               style="display : none;"/>
+                        <input type="text" class="MiFloraRemoteAttr form-control" data-l1key="remoteName"
+                               placeholder="{{Nom de l'antenne}}"/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">{{Ip}}</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="MiFloraRemoteAttr form-control" data-l1key="configuration"
+                               data-l2key="remoteIp"/>
+                    </div>
+                    <label class="col-sm-1 control-label">{{Port}}</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="MiFloraRemoteAttr form-control" data-l1key="configuration"
+                               data-l2key="remotePort"/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">{{User}}</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="MiFloraRemoteAttr form-control" data-l1key="configuration"
+                               data-l2key="remoteUser"/>
+                    </div>
+                    <label class="col-sm-1 control-label">{{Password}}</label>
+                    <div class="col-sm-3">
+                        <input type="password" class="MiFloraRemoteAttr form-control" data-l1key="configuration"
+                               data-l2key="remotePassword"/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">{{Device}}</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="MiFloraRemoteAttr form-control" data-l1key="configuration"
+                               data-l2key="remoteDevice" placeholder="{{ex : hci0}}"/>
+                    </div>
+                </div>
 
-						</fieldset>
-				</form>
-	</div>
+            </fieldset>
+        </form>
+    </div>
 </div>
 
 <script>
@@ -212,6 +225,7 @@ foreach ($remotes as $remote) {
                     },
                     dataType: 'json',
                     error: function (request, status, error) {
+                        // alert('Exception:', error);
                         handleAjaxError(request, status, error,$('#div_MiFloraRemoteAlert'));
                     },
                     success: function (data) {
