f61af878651a01dc757f27f7f7aff8992e37672b
Rene Jullien <rene.jullien@gmail.com>
2019-04-26 21:30:01 +0200

    warning dependances install

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -27,6 +27,7 @@ function MiFlora_install() {
 
 function MiFlora_update() {
     log::add('MiFlora', 'info', 'config - update started');
+    message::add('MiFlora', 'Cette mise à jour nécessite de relancer les dépendances (local + antennes) même si elles apparaissent vertes');
 
     $sql = file_get_contents(dirname(__FILE__) . '/install.sql');
     log::add('MiFlora', 'info', 'sql - '.$sql);
